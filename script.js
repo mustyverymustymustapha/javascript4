42148b59cce88b8fa75af3c70c54e4cac67a3b50
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-24 10:23:51 +0000

    idk

--- script.js
+++ script.js
@@ -33,3 +33,35 @@ function printFounderName() {
 }
 
 printFounderName();
+
+
+
+
+
+
+
+
+// -To Create an Array- //
+// An array is basically a list or collection of items in Javascript. A main way to create an array is to surround the items in square brackets. Here is an example:
+const arcadePrizes = ['Frameworks', 'IPads', 'MacBooks', 'Stickers', 'Soldering Irons']
+
+
+// -To Access an Array- //
+// To access an array, you can use the index number of the item you want to access. The index number starts at zero, so the first item is assigned to index 0. Here is an example:
+const planets = ['Mercury', 'Venus', 'Earth', 'Mars']
+console.log(planets[0]) // this will print mercury
+
+
+// -Update Elements- //
+// To update an element in an array, you can use the index number of the element you want to update. Here is an example:
+const OS = ['Windows', 'MacOS', 'Linux']
+OS[1] = 'IOS'
+
+
+
+// -Arrays with const and let- //
+// Arrays with const and let are almost the same. The difference is that const variables cannot be updated, while variables using let can. Here is an example:
+let rats = ['Cuddly', 'Scary', 'Disease Superspreaders']
+rats[2] = 'Awesome'
+// re-assigned
+
